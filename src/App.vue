8b8d8a329a8e13956172b2482caa2469a2e2eac4
huanghonghao <1250659520@qq.com>
2024-05-24 21:39:48 +0800

    update:202405242139

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,56 @@
 <script setup>
-import { onMounted, reactive, ref, toRaw } from "vue";
+import { computed, nextTick, onMounted, reactive, ref, toRaw } from "vue";
 import Artplayer from "./components/Artplayer.vue";
 import MonacoEditor from "./components/MonacoEditor.vue";
-import artplayerPluginDanmuku from 'artplayer-plugin-danmuku'
-import { layer } from "@layui/layer-vue"
-import flvjs from 'flv.js'
-import Hls from 'hls.js'
+import artplayerPluginDanmuku from "artplayer-plugin-danmuku";
+import { layer } from "@layui/layer-vue";
+import flvjs from "flv.js";
+import Hls from "hls.js";
+import { setStore, getStore } from './utils/store'
+import { debounce } from './utils/index'
 
-const urlSearchParams = new URLSearchParams(window.location.search)
+const urlSearchParams = new URLSearchParams(window.location.search);
 let art = undefined;
 let monaco = undefined;
-const labelPosition = ref('right');
-const dmFormat = ref(1)
+const labelPosition = ref("right");
+const dmFormat = ref(1);
 // 表单实例
 const layFormRef = ref(null);
+const tab = ref('2')
+const monacoValue = ref(getStore({name: 'playList'}) ?? '');
+const playList = computed(() => {
+  try {
+    if (!monacoValue.value) {
+      return []
+    }
+    const content = JSON.parse(monacoValue.value);
+    debounce(() => {
+      setStore({ name: 'playList', content: monacoValue.value})
+    }, 1000)();
+    return content
+  } catch (error) {
+    console.error(error)
+  }
+  return []
+})
 
 const model = reactive({
-  videoUrl: urlSearchParams.get('vUrl') || localStorage.getItem('videoUrl'),
-  dmUrl: urlSearchParams.get('dmUrl') || localStorage.getItem('dmUrl'),
+  videoUrl: urlSearchParams.get("vUrl") || localStorage.getItem("videoUrl"),
+  dmUrl: urlSearchParams.get("dmUrl") || localStorage.getItem("dmUrl"),
 });
 
 function playFlv(video, url, art) {
   if (flvjs.isSupported()) {
-    const flv = flvjs.createPlayer({ type: 'flv', url });
+    const flv = flvjs.createPlayer({ type: "flv", url });
     flv.attachMediaElement(video);
     flv.load();
 
     // optional
     art.flv = flv;
-    art.once('url', () => flv.destroy());
-    art.once('destroy', () => flv.destroy());
+    art.once("url", () => flv.destroy());
+    art.once("destroy", () => flv.destroy());
   } else {
-    art.notice.show = 'Unsupported playback format: flv';
+    art.notice.show = "Unsupported playback format: flv";
   }
 }
 
@@ -43,12 +62,12 @@ function playM3u8(video, url, art) {
 
     // optional
     art.hls = hls;
-    art.once('url', () => hls.destroy());
-    art.once('destroy', () => hls.destroy());
-  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    art.once("url", () => hls.destroy());
+    art.once("destroy", () => hls.destroy());
+  } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
     video.src = url;
   } else {
-    art.notice.show = 'Unsupported playback format: m3u8';
+    art.notice.show = "Unsupported playback format: m3u8";
   }
 }
 
@@ -61,8 +80,8 @@ const option = {
 };
 
 const style = reactive({
-  height: '550px'
-})
+  height: "550px",
+});
 
 function loadDm(danmuku) {
   const plugins = toRaw(art.plugins);
@@ -73,94 +92,98 @@ function loadDm(danmuku) {
     plugins.artplayerPluginDanmuku.load();
     return;
   }
-  plugins.add(artplayerPluginDanmuku({
-    // 弹幕数组
-    danmuku: danmuku,
-    fontSize: 22,
-    speed: 7,
-    antiOverlap: true,
-    useWorker: true, // 是否使用 web worker
-    synchronousPlayback: true, // 是否同步到播放速度
-    margin: [10, "50%"],
-  }))
+  plugins.add(
+    artplayerPluginDanmuku({
+      // 弹幕数组
+      danmuku: danmuku,
+      fontSize: 22,
+      speed: 7,
+      antiOverlap: true,
+      useWorker: true, // 是否使用 web worker
+      synchronousPlayback: true, // 是否同步到播放速度
+      margin: [10, "50%"],
+    })
+  );
 }
 
 const play = function () {
-  localStorage.setItem('videoUrl', model.videoUrl)
+  localStorage.setItem("videoUrl", model.videoUrl);
   // 识别视频类型
-  if (model.videoUrl.endsWith('.mp4')) {
-    art.type = 'mp4';
+  if (model.videoUrl.endsWith(".mp4")) {
+    art.type = "mp4";
   }
-  if (model.videoUrl.endsWith('.flv')) {
-    art.type = 'flv';
+  if (model.videoUrl.endsWith(".flv")) {
+    art.type = "flv";
   }
   setTimeout(() => {
     art.switchUrl(model.videoUrl);
     // art.url = model.videoUrl;
-  }, 500)
+  }, 500);
   if (!model.dmUrl) {
     return false;
   }
-  localStorage.setItem('dmUrl', model.dmUrl)
-  if (model.dmUrl.endsWith('.xml')) {
+  localStorage.setItem("dmUrl", model.dmUrl);
+  if (model.dmUrl.endsWith(".xml")) {
     loadDm(model.dmUrl);
   } else {
     fetch(model.dmUrl)
-      .then(res => res.json())
-      .then(json => {
+      .then((res) => res.json())
+      .then((json) => {
         switch (dmFormat.value) {
           case 1:
-            loadDm(json.danmuku.map(item => ({
-              text: item[4],
-              time: Number(item[0]),
-              color: '#fff',
-              border: false,
-              mode: item[1] === 'right' ? 0 : 1
-            })))
-            break
+            loadDm(
+              json.danmuku.map((item) => ({
+                text: item[4],
+                time: Number(item[0]),
+                color: "#fff",
+                border: false,
+                mode: item[1] === "right" ? 0 : 1,
+              }))
+            );
+            break;
         }
         // eval(monaco.getValue())
       })
       .catch((error) => {
-        console.error('弹幕加载失败，url:', model.dmUrl, error);
+        console.error("弹幕加载失败，url:", model.dmUrl, error);
         layer.notifiy({
           title: "弹幕获取失败",
           content: error.message,
-          icon: 2
-        })
-      })
+          icon: 2,
+        });
+      });
   }
 };
 
 // 重置表单
 const reset = function () {
   layFormRef.value.reset();
-}
+};
 
 // 初始化播放器实例，绑定事件
 function getArtInstance(instance) {
   art = instance;
-  art.on('ready', () => {
+  art.on("ready", () => {
     console.info(art.url);
     art.play();
   });
-  art.on('restart', () => {
-    console.info('restart');
+  art.on("restart", () => {
+    console.info("restart");
     art.play();
   });
-  art.on('url', (url) => {
-    art.notice.show = '视频加载中......';
+  art.on("url", (url) => {
+    art.notice.show = "视频加载中......";
   });
-  art.on('error', (error, reconnectTime) => {
+  art.on("error", (error, reconnectTime) => {
     console.info(error, reconnectTime);
-    art.notice.show = "加载失败：" + error + " 重连ing..." + reconnectTime
+    art.notice.show = "加载失败：" + error + " 重连ing..." + reconnectTime;
   });
-  art.on('artplayerPluginDanmuku:error', (error) => {
+  art.on("artplayerPluginDanmuku:error", (error) => {
     layer.notifiy({
       title: "弹幕加载失败",
       content: error.message,
-      icon: 2
-    })
+      icon: 2,
+    });
   });
 }
 
@@ -168,6 +191,13 @@ function getMonacoInstance(instance) {
   monaco = instance;
 }
 
+const handlePlayListChange = value => {
+  model.videoUrl = value[0]
+  model.dmUrl = value[1]
+  nextTick(() => {
+    play()
+  })
+}
 </script>
 
 <template>
@@ -181,23 +211,34 @@ function getMonacoInstance(instance) {
     </lay-row>
     <lay-row>
       <lay-col md="24">
-        <lay-panel style="padding: 20px;">
-          <lay-form :model="model" ref="layFormRef" :pane="true">
-            <lay-form-item label="播放链接" :label-position="labelPosition" prop="videoUrl" >
-              <lay-input v-model="model.videoUrl" :allow-clear="true"></lay-input>
-            </lay-form-item>
-            <lay-form-item label="弹幕链接" :label-position="labelPosition" prop="dmUrl" >
-              <lay-input v-model="model.dmUrl" :allow-clear="true"></lay-input>
-            </lay-form-item>
-            <lay-button type="normal" @click="play">播放</lay-button>
-            <lay-button @click="reset">重置</lay-button>
-          </lay-form>
+        <lay-panel style="padding: 20px">
+          <lay-tab v-model="tab">
+            <lay-tab-item title="单集" id="1">
+              <lay-form :model="model" ref="layFormRef" :pane="true">
+                <lay-form-item label="播放链接" :label-position="labelPosition" prop="videoUrl">
+                  <lay-input v-model="model.videoUrl" :allow-clear="true"></lay-input>
+                </lay-form-item>
+                <lay-form-item label="弹幕链接" :label-position="labelPosition" prop="dmUrl">
+                  <lay-input v-model="model.dmUrl" :allow-clear="true"></lay-input>
+                </lay-form-item>
+                <lay-button type="normal" @click="play">播放</lay-button>
+                <lay-button @click="reset">重置</lay-button>
+              </lay-form>
+            </lay-tab-item>
+            <lay-tab-item title="专辑" id="2">
+              <lay-select style="width: 100%;margin-bottom: 20px;" @change="handlePlayListChange">
+                  <lay-select-option v-for="(item, index) in playList" :key="index" :value="[item.url, item.dmUrl]"
+                    :label="item.label"></lay-select-option>
+                </lay-select>
+                <MonacoEditor v-model="monacoValue" language="json" />
+            </lay-tab-item>
+          </lay-tab>
         </lay-panel>
       </lay-col>
     </lay-row>
     <lay-row>
       <lay-col md="24">
-        <lay-panel style="padding: 20px;">
+        <lay-panel style="padding: 20px">
           <lay-form-item label="弹幕转换" :label-position="labelPosition">
             <lay-select v-model="dmFormat" placeholder="弹幕转换">
               <lay-select-option :value="1" label="jplayer"></lay-select-option>
--- src/components/MonacoEditor.vue
+++ src/components/MonacoEditor.vue
@@ -4,16 +4,29 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 const editorRef = ref(null);
 const editorInstance = ref(null);
-const emit = defineEmits(['getInstance'])
+const emit = defineEmits(['getInstance', 'update:modelValue'])
+const prop = defineProps({
+	language: String,
+	modelValue: {
+		type: String,
+		default: ''
+	}
+})
 
 onMounted(() => {
 	if (editorRef.value && !editorInstance.value) {
 		editorInstance.value = monaco.editor.create(editorRef.value, {
-			value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-			language: 'javascript',
+			value: prop.modelValue,
+			language: prop.language,
 			lineNumbers: true,
-			theme: 'vs-dark'
+			theme: 'vs-dark',
+			tabSize: 2,
+			minimap: { enabled: false },
 		});
+
+		editorInstance.value.onDidChangeModelContent(v => {
+			emit('update:modelValue', toRaw(editorInstance.value).getValue())
+		})
 	}
 	nextTick(() => {
 		emit('getInstance', toRaw(editorInstance.value))
@@ -30,6 +43,6 @@ onUnmounted(() => toRaw(editorInstance.value)?.dispose());
 <style scoped>
 .editor {
 	/* width: 100%; */
-	height: 300px;
+	height: 100px;
 }
 </style>
